ea118797494475deb9cb1ec7d7c1d8ed44ac52a7
Tirlock <shmulevich0507@mail.ru>
2025-05-03 13:44:00 +0300

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -22,13 +22,13 @@ class GameSprite(sprite.Sprite):
 # класс главного игрока
 class Player(GameSprite):
    # метод для управления спрайтом стрелками клавиатуры
-    def update(self):
+    def update_r(self):
         keys = key.get_pressed()
         if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
         if keys[K_DOWN] and self.rect.y < win_width - 80:
             self.rect.y += self.speed
- def update_l(self):
+    def update_l(self):
         keys = key.get_pressed()
         if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
@@ -44,8 +44,8 @@ finish = False
 clock = time.Clock()
 FPS = 60
 
-racket1 = Player('racket1.png', 30, 200, 4, 50, 150)
-racket2 = Player('racket2.png', 30, 200, 4, 50, 150)
+racket1 = Player('racket1.jpg', 30, 200, 4, 50, 150)
+racket2 = Player('racket2.jpg', 30, 200, 4, 50, 150)
 ball = GameSprite('ball.png', 200, 200, 4, 50, 50)
 
 font.init()
@@ -57,32 +57,32 @@ speed_x = 3
 speed_y = 3
 
 while game:
- for e in event.get():
-  if e.type == QUIT:
-   game = False
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
 
- if finish != True:
-  window.fill(back)
-  racket1.update_1()
-  racket1.update_r()
-  ball.rect.x += speed_x
-  ball.rect.y += speed_y
+    if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        racket1.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
 
-if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
- speed_x *= -1
+    if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+        speed_x *= -1
 
-if ball.rect.y > win_height - 50 or ball.rect.y < 0
-speed_y *= -1
+    if ball.rect.y > win_height - 50 or ball.rect.y < 0:
+        speed_y *= -1
 
-if ball.rect.x < 0:
- finish = True
- window.blit(lose1, (200, 200))
- game_over = True
+    if ball.rect.x < 0:
+        finish = True
+        window.blit(lose1, (200, 200))
+        game_over = True
 
-if ball.rect.x > win_width:
- finish = True
- window.blit(lose2, (200, 200))
- game_over = True
+    if ball.rect.x > win_width:
+        finish = True
+        window.blit(lose2, (200, 200))
+        game_over = True
 
 racket1.reset()
 racket2.reset()
@@ -90,7 +90,7 @@ ball.reset()
 
 display.update()
 clock.tick(FPS)
-                    
+                 
 
 
 
